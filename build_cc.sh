0f7768e0a23cf92cac52e42f1a97514a08f88a9d
jikui <j.pei@f5.com>
2020-03-15 22:38:28 +0800

    new branch

---
+++ build_cc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+tgtdir=/Users/pei/nginx
+#tgtdir=/usr/local/nginx
+cdir=`cd $(dirname $0); pwd`
+(
+    #cd $cdir/nginx-1.16.1
+    cd $cdir
+    set -e
+    for option; do
+        case $option in
+            conf*)
+                # ./configure --prefix=$tgtdir
+                #./configure --with-debug --with-stream --with-stream_ssl_module --with-cc=/usr/bin/cc --with-cc-opt='-O0 -g' --prefix=$tgtdir 
+                ./configure --with-debug --with-stream --with-cc=/usr/bin/cc --with-cc-opt='-O0 -g' --prefix=$tgtdir 
+                ;;
+            make)
+                #bear make
+                make
+                ;;
+            install)
+                make install
+                ;;
+            *)
+                echo "$0 [conf[igure]|make|install]"
+                ;;
+        esac
+    done
+    set +e
+)
